30f9cd37e8839617b7e586afe20480f6b4e22e1d
charlespylee <charlespylee@gmail.com>
2014-10-19 18:27:08 -0400

    commit1

--- src/NetworkAnalysis.java
+++ src/NetworkAnalysis.java
@@ -11,7 +11,7 @@ public class NetworkAnalysis {
    //  Database credentials
    static final String USER = "root";
    static final String PASS = "charles";
-   static boolean[] marked;
+   static boolean[] visited;
    static int count;
 
    public static void main(String[] args) {
@@ -24,18 +24,19 @@ public class NetworkAnalysis {
 		      
 		      //Construct Hash Table (FromNode -> MappedNode)
 		      ArrayList<ArrayList<Integer>> nodeTable;
-		      nodeTable = ConstructTable(conn);
+		      nodeTable = constructTable(conn);
 		      
 		      
 		      if (method.equals("NeighbourCount")){
 				System.out.println(NeighbourCount(Integer.parseInt(parameter),conn));
 			  }else if (method.equals("ReachabilityCount")){
 //				System.out.println(ReachabilityCount(Integer.parseInt(parameter),conn));
-			  }else if (method.equals("DiscoverCliques")){
-				System.out.println(DiscoverCliques(Integer.parseInt(parameter),conn, nodeTable));
-			  }else if (method.equals("NetworkDiameter")){
-				System.out.println(NetworkDiameter(Integer.parseInt(parameter),conn, nodeTable));
 			  }
+//			  else if (method.equals("DiscoverCliques")){
+//				System.out.println(DiscoverCliques(Integer.parseInt(parameter),conn, nodeTable));
+//			  }else if (method.equals("NetworkDiameter")){
+//				System.out.println(NetworkDiameter(Integer.parseInt(parameter),conn, nodeTable));
+//			  }
 		      
 
 		   }catch(SQLException se){
@@ -86,7 +87,7 @@ public class NetworkAnalysis {
    
    //Construct Hash Table for Each Node(FromNode -> endNode)
    
-   public static ArrayList<ArrayList<Integer>> ConstructTable(Connection conn){
+   public static ArrayList<ArrayList<Integer>> constructTable(Connection conn){
 	   //construct tmpResult, get length and construct empty ArrayList
 	   ArrayList<ArrayList<Integer>> tmpResult = new ArrayList<ArrayList<Integer>>();
 	   
@@ -128,7 +129,7 @@ public class NetworkAnalysis {
 	   String sql2 = "SELECT FromNodeID FROM roadNet_CA WHERE EndNodeID="+ id;
 	   ResultSet rs = conn.createStatement().executeQuery(sql);
 	   ResultSet rs2 = conn.createStatement().executeQuery(sql2);
-	   //caculate the number of row
+	   //calculate the number of row
 	   rs.last();
 	   rs2.last();
 	   int sql_length=rs.getRow();
@@ -170,149 +171,50 @@ public class NetworkAnalysis {
 	   return count;
    }
   
-   private void dfs(int[] a, int[] b, int rootNode,Connection conn, int step) {
-//       count++;
-//       marked[rootNode] = true;
-//       for (int w : nextNodes(rootNode,conn)){
-//           if (!marked[w]) {
-//               dfs(G, w, step+1);
-//           }
-//       }
-	   ArrayList<Struct>
-	   Struct{
-		   int steps;
-		   Integer Val;
-		   
-	   }
-   }
-/*   public static int ReachabilityCount(int id, Connection conn){
-//	   int[] nodes=nextNodes(id,conn);
-//	   ArrayList reachableNodes=new ArrayList();
-	   ArrayList<>
-	   //Execute a query
-	   String sql = "SELECT EndNodeID FROM roadNet_CA";
-	   String sql2 = "SELECT FromNodeID FROM roadNet_CA";
-	   ResultSet rs = conn.createStatement().executeQuery(sql);
-	   ResultSet rs2 = conn.createStatement().executeQuery(sql2);
-	   //caculate the number of row
-	   rs.last();
-	   rs2.last();
-	   int sql_length=rs.getRow();
-	   int sql2_length=rs2.getRow();
-	   rs.beforeFirst();
-	   rs2.beforeFirst();
-	   
-	   int [] EndNodes = new int[sql_length];
-	   int [] FromNodes = new int[sql2_length];
-	   
-	   System.out.print(nodes.length); 
-	   
-	   return 0;
-   }*/
-   
-   
-//********************************Combination Helper Functions for Discover Cliques******************************
-   public static void combineFunc_helper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> fromList, int start, ArrayList<Integer> tmpResult, int target){
-	   if(tmp.size() == k) result.add(tmp);
-	   else{
-		   for(int i = start; i < n-target+1; i++){
-			   ArrayList<Integer> tmp = (ArrayList<Integer>) tmpResult.clone();
-			   tmp.add(fromList.get(i));
-			   combineFunc_helper(result, fromList, start, tmp, target-1);
-		   }
-	   }
-   }
-   
-   public static ArrayList<ArrayList<Integer>> combineFunc(ArrayList<Integer> fromList, int k){
-	   ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-	   combineFunc_helper(result, fromList, 0, new ArrayList<Integer>(), k);
-	   return result;
-   }
-   
-   
-   public static ArrayList<ArrayList<Integer>> DiscoverCliques(int k, Connection conn){
-	   //construct 2d ArrayList, store all combinations
-	   ArrayList<ArrayList<Integer>> combResult = new ArrayList<ArrayList<Integer>>();
-	   //construct the linked_table at first
-	   ArrayList<ArrayList<Integer>> linkTable = ConstructTable(conn);
-	   //construct the valid subgraphs(Cliques)
-	   ArrayList<ArrayList<Integer>> cliqueResult = new ArrayList<ArrayList<Integer>>();
-	   
-	   try{
-		   //Execute a query
-		   String fromSqlDis = "SELECT DISTINCT FromNodeId FROM roadNet_CA";
-		   ResultSet fromDisSet = conn.createStatement().executeQuery(fromSqlDis);
-		   
-		   //travel from set, get ArrayList of FromNode
-		   ArrayList<Integer> fromNodeList = new ArrayList<Integer>();
-		   while(fromDistSet.next()){
-			   fromNodeList.add(fromDisSet.getInt("FromNodeID"));
-		   }
-		   
-		   //find k-combination
-		   combResult = combineFunc(fromNodeList, k);
-		   
-		   //after getting combResult, go over the combResult and insert the valid combination
-		   for(int i = 0; i < combResult.size(); i++){
-			   boolean validComb = true;
-			   ArrayList<Integer> thisRow = combResult.get(i);
-			   for(int j = 0; j < thisRow.size(); j++){
-				   //Undirected!!!!!!!!!
-				   int rowVal = thisRow.get(i);
-				   for(int m = 0; m < thisRow.size() && m != j; m++){
-					   int tarVal = thisRow.get(m);
-					   if(linkTable[rowVal].indexOf(tarVal) == -1) validCom = false;
-				   }
-			   }
-			   if(validComb) cliqueTable.add(thisRow));
-		   }
-		   
-		 }catch(SQLException se){
-		   //Handle errors for JDBC
-		   se.printStackTrace();
-		 }
-	      
-	   return cliqueTable;
-	   
-	   //verify the valid combination, delete if not valid
-//	   return result;
-   }
-   
-//*******************************************Discover Cliques End********************************************
 
-   
-
-//*******************************************NetworkDiameter Start*******************************************
-   public static int NetworkDiameter(int id, Connection conn){
-	   int resultDia = 0;
-	   //construct the linked_table at first
-	   ArrayList<ArrayList<Integer>> linkTable = ConstructTable(conn);
+   private static void dfs(ArrayList<ArrayList<Integer>> nodeTable, int rootNode) {
 	   
-	   try{
-		   //Execute a query
-		   String fromSqlDis = "SELECT DISTINCT FromNodeId FROM roadNet_CA";
-		   ResultSet fromDisSet = conn.createStatement().executeQuery(fromSqlDis);
-		   
-		   //calculate the number of row; push empty rows to tmpResult
-		   fromDisSet.last(); Integer arrSize=fromDisSet.getRow(); fromDisSet.beforeFirst();
-		   for(int i = 0; i < arrSize; i++){
-			   ArrayList<Integer> tmp = new ArrayList<Integer>();
-			   tmpResult.add(tmp);
-		   }
-		   
-		   //travel both fromSet and endSet, create hashTable
-		   while(fromSet.next() && endSet.next()){
-			   tmpResult.get(fromSet.getInt("FromNodeID")).add(endSet.getInt("EndNodeID"));
-		   }
-		   
-		   
-		 }catch(SQLException se){
-		   //Handle errors for JDBC
-		   se.printStackTrace();
-		 }
+	   ArrayList <Integer> neighbor = nodeTable.get(rootNode);
+
+	   if (neighbor.size()>0) {
+	       visited [rootNode] = true;
+	       
+	       for (int w : neighbor){
+	           if (!visited[w]) {
+	        	   count++;
+	               dfs(nodeTable, w);	     
+	           }
+	       }
+	   }  
+       return;
+   }
+ 
+   public static int ReachabilityCount(int id, Connection conn){
+	   ArrayList<ArrayList<Integer>> nodeTable;
+	   nodeTable = constructTable(conn);
+	   count=0;
+	   dfs(nodeTable,id);
+	   System.out.print(count); 
+	   
+	   return count;
    }
    
-//*******************************************NetwordDiameter End*********************************************
+//   public static ArrayList<ArrayList<Integer>> DiscoverCliques(int k, Connection conn){
+////	   ArrayList<ArrayList<Integer>> result;
+////	   //find all combinations
+////	   
+////	   
+////	   //verify the valid combination, delete if not valid
+////	   return result;
+//   }
+   
+//   public static int NetworkDiameter(int id, Connection conn){
+////	   //Execute a query
+////	   String sql = "SELECT EndNodeID FROM roadNet_CA";
+////	   String sql2 = "SELECT FromNodeID FROM roadNet_CA";
+////	   ResultSet rs = conn.createStatement().executeQuery(sql);
+////	   ResultSet rs2 = conn.createStatement().executeQuery(sql2);
+//   }
 
 }
 	
